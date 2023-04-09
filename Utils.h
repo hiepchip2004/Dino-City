5dc1c2d4fa84bb7f94363185cc89d4b731b49ec8
hiepchip2004 <124542089+hiepchip2004@users.noreply.github.com>
2023-04-10 06:51:59 +0700

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "CommonFunct.h"
 #include "Button.h"
@@ -32,19 +32,14 @@ void HandlePlayButton(SDL_Event* e,
     bool& Play,
     Mix_Chunk* gClick);
 
-void HandleHelpButton(SDL_Event* e,
+void HandleIntroButton(SDL_Event* e,
     SDL_Rect(&gBackButton)[BUTTON_TOTAL],
-    Button& HelpButton,
+    Button& IntroButton,
     Button& BackButton,
-    LTexture gInstructionTexture,
+    LTexture gIntroTexture,
     LTexture gBackButtonTexture,
     SDL_Renderer* gRenderer,
     bool& Quit_game,
-    Mix_Chunk* gClick);
-
-void HandleExitButton(SDL_Event* e,
-    Button& ExitButton,
-    bool& Quit,
     Mix_Chunk* gClick);
 
 void HandleContinueButton(Button ContinueButton,
@@ -94,6 +89,7 @@ void DrawPlayerScore(LTexture gTextTexture,
     TTF_Font* gFont,
     const int& score);
 
+//tính điểm cao nhất
 void DrawPlayerHighScore(LTexture gTextTexture,
     LTexture gHighScoreTexture,
     SDL_Color textColor,
